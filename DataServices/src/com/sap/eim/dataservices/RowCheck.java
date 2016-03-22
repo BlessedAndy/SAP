8e1591b2078133df07eaef3cbd4e724f5e1efda9
Andy Zhang <shumaozhang@yahoo.omc>
2016-03-22 16:18:55 +0800

    Add class to tackle target and source row count.

---
+++ DataServices/src/com/sap/eim/dataservices/RowCheck.java
@@ -0,0 +1,16 @@
+package com.sap.eim.dataservices;
+
+/**
+ * 
+ * @author Andy Zhang
+ * 
+ * Check whether the target row count equals the source row count.
+ *
+ */
+public class RowCheck {
+	
+	public int rowCount(String tableName){
+		return 0;
+	}
+
+}
