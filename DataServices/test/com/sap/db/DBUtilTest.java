459003a0bd2d5dd145353051fd249d80dcf57945
Andy Zhang <shumaozhang@yahoo.omc>
2016-03-23 17:52:45 +0800

    add oracle support

---
+++ DataServices/test/com/sap/db/DBUtilTest.java
@@ -0,0 +1,40 @@
+package com.sap.db;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DBUtilTest {
+
+	@Test
+	public void testGetConnection() {
+//		System.out.println(DBUtil.getHANAConnection().toString());
+		System.out.println(HanaUtil.getConnection().toString());
+	}
+
+	@Test
+	public void testExecuteupdate2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExecuteUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPreparedStatement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetQueryResultSet() {
+		fail("Not yet implemented");
+	}
+
+}
